bc5a7c93a96dda9d448f1eb71fd38bcd51e277d6
dadko44 <dawid.kuczer.info@gmail.com>
2021-07-27 14:56:35 +0200

    minor fix

--- js/index.js
+++ js/index.js
@@ -3,15 +3,23 @@ let desktopSharePanel = document.getElementById('desktop-share-panel');
 let shareIcon = document.getElementById('share-icon');
 let shareBttn = document.getElementById('share-bttn');
 
+let toggleSharePanel = function(){
+    if(shareIcon.classList.contains('active-share-icon')){
+        hideSharePanel();
+    }
+    else{
+        showSharePanel();
+    }
+};
 let showSharePanel = function(){
     shareIcon.classList.add('active-share-icon');
     shareBttn.classList.add('active-share-bttn');
     mobileSharePanel.classList.add('share-panel-add-height');
     desktopSharePanel.classList.add('share-panel-add-height');
-}
+};
 let hideSharePanel = function(){
     shareIcon.classList.remove('active-share-icon');
     shareBttn.classList.remove('active-share-bttn');
     mobileSharePanel.classList.remove('share-panel-add-height');
     desktopSharePanel.classList.remove('share-panel-add-height')
-}
+};
